16b36952e1043c733f3b43232a55635d3ad4a977
Safet Safet <safetsunay@gmail.com>
2026-03-06 18:33:13 +0200

    Bump SW cache v3, skipWaiting + clients.claim for instant updates

--- sw.js
+++ sw.js
@@ -1,9 +1,13 @@
-const CACHE = 'momentum-v2';
+const CACHE = 'momentum-v3';
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./', './index.html', './manifest.webmanifest', './sw.js'])));
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => clients.claim())
+  );
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
